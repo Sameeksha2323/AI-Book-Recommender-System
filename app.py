66b14241b1ac316a028562e8b53d8856887507b0
Sameeksha <146654556+Sameeksha2323@users.noreply.github.com>
2025-02-19 17:47:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    new_website_url = "https://ai-book-recommendation-project.vercel.app/"  # <--- REPLACE WITH YOUR ACTUAL NEW URL
+
+    return render_template('index.html', new_website_url=new_website_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
