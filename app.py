142873c392449cb3d24924e0b4cb993d4e2022d0
Sameeksha <146654556+Sameeksha2323@users.noreply.github.com>
2025-02-25 01:24:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    new_website_url = "https://ai-book-recommendation-project.onrender.com/"  # <--- REPLACE WITH YOUR ACTUAL NEW URL
+    new_website_url = "https://ai-book-recommendation-project.vercel.app/"  # <--- REPLACE WITH YOUR ACTUAL NEW URL
 
     return render_template('index.html', new_website_url=new_website_url)
 
